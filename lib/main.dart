d0b3a9137b7c97dda1653fa8e5f0e018bbe88b70
HEMASAMIR <ibrahimsameer78@icloud.com>
2023-04-23 17:36:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/feature/presentation/view/splash_view/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: const Color(0xff100b20)),
+      home: const SplashView(),
+    );
+  }
+}
